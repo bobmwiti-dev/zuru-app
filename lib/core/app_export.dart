4b928632680637d6dd591faf71e774ae502f1549
Bobbin Mwiti Miriti <bobbinmwitidev@gmail.com>
2026-01-09 23:25:41 +0300

    chore: update app configuration and dependencies
    - Update app export configuration for new modules
    - Add missing dependencies to pubspec.yaml
    - Configure app constants and module exports

--- lib/core/app_export.dart
+++ lib/core/app_export.dart
@@ -1,8 +1,31 @@
 // This file is used to export all the necessary files and packages for the app
 
-export 'package:connectivity_plus/connectivity_plus.dart';
-export 'package:google_fonts/google_fonts.dart';
+// Constants
+export 'constants/app_colors.dart';
+export 'constants/app_strings.dart';
+export 'constants/app_sizes.dart';
+
+// Utilities
+export 'utils/animation_utils.dart';
+
+// Routes
 export '../routes/app_routes.dart';
+
+// Widgets
+export '../widgets/custom_app_bar.dart';
+export '../widgets/custom_bottom_bar.dart';
+export '../widgets/custom_button.dart';
 export '../widgets/custom_icon_widget.dart';
 export '../widgets/custom_image_widget.dart';
+export '../widgets/custom_loading_indicator.dart';
+export '../widgets/shimmer_widget.dart';
+
+// Re-export shimmer classes for convenience
+export '../widgets/shimmer_widget.dart' show ShimmerContainer, ShimmerText, ShimmerListItem;
+
+// Theme
 export '../theme/app_theme.dart';
+
+// External packages
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:google_fonts/google_fonts.dart';
